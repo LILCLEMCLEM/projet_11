ec43aa1983728c0d624ae80bf1d69fcb5ae9db0a
Clément L <85962184+LILCLEMCLEM@users.noreply.github.com>
2023-11-13 11:42:56 +0100

    update

--- src/components/Wide_img.js
+++ src/components/Wide_img.js
@@ -9,7 +9,7 @@ function WideImg({ image_src, text, type }) {
         <img src={image_src} alt="wide comp" className="img_container_h"></img>
       )}
 
-      {text !== "" && window.innerWidth > 720 ? (
+      {(text !== "") || (window.innerWidth > 720) ? (
         <p className="img_text">{text}</p>
       ) : (
         <p className="img_text">
